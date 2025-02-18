3f6871199454a4dc27a8a28fdc4eef44b0f3ef4d
jagathjayan <jagathjayan016@gmail.com>
2025-02-18 15:09:27 +0530

    Improved

--- hlo.c
+++ hlo.c
@@ -2,9 +2,9 @@
 int main
 {
 	 int a , b , c ;
-	printf("enter 1st number");
+	printf("enter 1st number: ");
 	scanf("%d" & a);
-	printf("enter 2nd number");
+	printf("enter 2nd number: ");
 	scanf("%d" & b);
 	c=a+b;
 	scanf("sum is %d" &c);
